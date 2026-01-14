c557abf2257b7068a469d970fa51d500c65ccf2a
jm1021 <jmort1021@gmail.com>
2026-01-14 12:36:50 -0800

    updating link again

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -8,7 +8,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8303";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://innovators.opencodingsociety.com/";
+    pythonURI = "https://innovators.opencodingsociety.com";
 
 }
 
